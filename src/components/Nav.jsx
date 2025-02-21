d73bb3c0fc7edb188c90e2a1e7c2adbca4386e7c
riomar001 <inguitomario00@gmail.com>
2025-02-21 15:04:13 +0800

    another push

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className="flex flex-row justify-between items-center border border-neutral-300 max-w-7xl">
       <h1>My Notes</h1>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>Add To Do's</li>
-        </ul>
-      </div>
+      <ul className="flex flex-row gap-x-5">
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to={"/"}>Add to Do's</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
